863cb8206ffaaed2a94e72029379168211bf1960
Jakob <runetynan@gmail.com>
2016-01-16 18:51:25 -0500

    Added ExampleClawArm, ExampleDrive. Added motors, recycled drive code, built RobotMap, updated Robot, Built single OI button.

--- Example2016/src/org/usfirst/frc/team340/robot/subsystems/ExampleClawArm.java
+++ Example2016/src/org/usfirst/frc/team340/robot/subsystems/ExampleClawArm.java
@@ -1,19 +1,30 @@
 
 package org.usfirst.frc.team340.robot.subsystems;
 
+import org.usfirst.frc.team340.robot.RobotMap;
+
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.TalonSRX;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
  *
  */
-public class ExampleSubsystem extends Subsystem {
+public class ExampleClawArm extends Subsystem {
     
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
+	
+	TalonSRX leftMotor = new TalonSRX(RobotMap.ClawLeftMotor);
+	TalonSRX rightMotor = new TalonSRX(RobotMap.ClawRightMotor);
+	
+	Solenoid leftPiston = new Solenoid(0);
 
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
     }
+    
+    
 }
 
---
+++ Example2016/src/org/usfirst/frc/team340/robot/subsystems/ExampleDrive.java
@@ -0,0 +1,121 @@
+package org.usfirst.frc.team340.robot.subsystems;
+
+import org.usfirst.frc.team340.robot.RobotMap;
+
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.TalonSRX;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ */
+public class ExampleDrive extends Subsystem {
+    
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+	
+	// Drive motors
+	private TalonSRX leftDrive;
+	private TalonSRX rightDrive;
+	
+	// Drive speed variables
+	public double leftMotorSpeed;
+	public double rightMotorSpeed;
+	
+	// Encoders on motors
+	private Encoder leftDriveEncoder;
+	private Encoder rightDriveEncoder;
+	
+	public ExampleDrive() {
+		leftDrive = new TalonSRX(RobotMap.DriveLeftMotor);
+		rightDrive = new TalonSRX(RobotMap.DriveRightMotor);
+		
+		leftDriveEncoder = new Encoder(RobotMap.DriveLeftEncoder1,RobotMap.DriveLeftEncoder2);
+		rightDriveEncoder = new Encoder(RobotMap.DriveRightEncoder1,RobotMap.DriveRightEncoder2);
+	}
+	
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+    
+    public void resetEncoder(){
+    	leftDriveEncoder.reset();
+    	rightDriveEncoder.reset();
+    }
+    
+    public double getLeftEncoder(){
+    	return leftDriveEncoder.get();
+    }
+    
+    public double getRightEncoder(){
+    	return rightDriveEncoder.get();
+    }
+    
+    public void setLeftDrive(double speed) {
+    	if(speed > 1) {
+    		speed = 1;
+    	} else if(speed < -1) {
+    		speed = -1;
+    	}
+    	leftDrive.set(speed);
+    }
+    
+    public void setRightDrive(double speed) {
+    	if(speed > 1) {
+    		speed = 1;
+    	} else if(speed < -1) {
+    		speed = -1;
+    	}
+    	rightDrive.set(speed);
+    }
+    
+    public void setLeftRightMotorOutputs(double leftOutput, double rightOutput){
+    	setLeftDrive(leftOutput);
+    	setRightDrive(-rightOutput);
+    }
+    
+    public void arcadeDrive(double moveValue, double rotateValue){
+    	if (moveValue >= 0.0){
+    		moveValue = (moveValue * moveValue);	
+   		} else {
+   			moveValue = -(moveValue * moveValue);
+   			
+   		}
+    	if (rotateValue >= 0.0){
+    		rotateValue = (rotateValue * rotateValue);
+    	} else {
+    		rotateValue = -(rotateValue *rotateValue);
+    	}
+    	if (moveValue > 0.0) 
+        {
+        
+            if (rotateValue > 0.0) 
+            {
+                leftMotorSpeed = moveValue - rotateValue;
+                rightMotorSpeed = Math.max(moveValue, rotateValue);
+            } 
+        
+            else 
+            {
+                leftMotorSpeed = Math.max(moveValue, -rotateValue);
+                rightMotorSpeed = moveValue + rotateValue;
+            }
+        }
+        
+        else 
+        {
+            if (rotateValue > 0.0) 
+            {
+                leftMotorSpeed = -Math.max(-moveValue, rotateValue);
+                rightMotorSpeed = moveValue + rotateValue;
+            }
+            else {
+                leftMotorSpeed = moveValue - rotateValue;
+                rightMotorSpeed = -Math.max(-moveValue, -rotateValue);
+            }
+        }
+    	setLeftRightMotorOutputs(leftMotorSpeed, rightMotorSpeed);
+   	}
+}
+
